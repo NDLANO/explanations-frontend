63fcc29f2d77a9add97d98a5ae16b75145cfbb1d
Sindre Nerdal <sindre.nerdal@capgemini.com>
2018-10-31 14:31:31 +0100

    Used label as placeholder in textarea

--- client/src/containers/CRUD/components/TextArea.js
+++ client/src/containers/CRUD/components/TextArea.js
@@ -3,7 +3,7 @@ import React from "react";
 const TextArea = ({value, type="text", id, label, onChange,className, isReadOnly=false, rows=5, columns=50}) => (
     <div  className={className}>
         <label htmlFor={id}>{label}</label>
-        <textarea rows={rows} cols={columns} value={value} id={id} onChange={onChange} placeholder={id} readOnly={isReadOnly} />
+        <textarea rows={rows} cols={columns} value={value} id={id} onChange={onChange} placeholder={label} readOnly={isReadOnly} />
     </div>
 );
 export default TextArea;
